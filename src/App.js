f45bf2d2b40e44890fe91423b30022cc065882b8
Jorge Gonzalez <jorge.j.gonzalez.93@gmail.com>
2018-12-27 17:16:45 +0100

    first lines react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default class CharacterCard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        console.log("did mount");
+
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("did update");
+
+    }
+    componentWillUnmount() {
+        console.log("willMount");
+
+    }
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log("shouldComponent Update ");
+
+    }
+     getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("getSnapShot");
+
+    }
+    render() {
+        console.log("render");
+        return (
+                <div className="card">ç
+        {/*unas veces se muestra y otras no*/}
+
+                    {this.state.app ? <CharterTitle/> : <span></span> }
+                    <p>Estado del personaje</p>
+                    <p>Género</p>
+                    <p>Capítulo</p>
+                </div>
+        );
+    }
 }
 
-export default App;
+class CharterTitle extends Component {
+    render() {
+        return (
+                <h1>Character Name </h1>
+        );
+    }
+}
